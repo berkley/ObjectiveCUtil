b926c04c180775cca99780bb5fdae5eca84a1835
Chad Berkley <cberkley@flightstats.com>
2012-01-24 14:57:35 -0800

    formatting .h file

--- CommonUtil.h
+++ CommonUtil.h
@@ -18,8 +18,27 @@
 
 @interface CommonUtil : NSObject
 
-//format a date to a string using the given format
-+ (NSString*)formatDate:(NSDate*)date withFormat:(NSString*)format;
+////////////////////// File Util //////////////////////////////////////////
+
+//return true if a file exists at the given path
++ (BOOL)fileExists:(NSString*)filePath;
+
+//find the documents directory for the app and return the path + the filename
++ (NSString*)getDataPathForFileWithName:(NSString*)filename;
+
+
+
+///////////////////// UI Util /////////////////////////////////////////////
+
+//returns the height of a UILabel based on the content of the label, the font and the width of the view
++ (float)getUILabelHeightByString:(NSString*)myString withFont:(UIFont*)font viewWidth:(int)myWidth;
+
+//get a UIImage from a url string
++ (UIImage*)imageFromUrl:(NSString*)url;
+
+
+
+////////////////// Dictionary Util ////////////////////////////////////////
 
 //translate a '/' delimited path to get a value from a nested dictionary
 //returns nil if the path is not found
@@ -29,26 +48,9 @@
 //returns @"" if the path is not found
 + (NSString*)getStringFromPath:(NSString*)path FromDictionary:(NSDictionary*)dict;
 
-//return the string response from a string formatted url
-+ (NSString*)getStringFromURLString:(NSString*)url;
 
-//return true if a file exists at the given path
-+ (BOOL)fileExists:(NSString*)filePath;
 
-//get a UIImage from a url string
-+ (UIImage*)imageFromUrl:(NSString*)url;
-
-//find the documents directory for the app and return the path + the filename
-+ (NSString*)getDataPathForFileWithName:(NSString*)filename;
-
-//write data to a file with the given filename
-+ (void)writeString:(NSString*)data ToFile:(NSString*)filename;
-
-//return a string with the frame params.  Useful for debugging.
-+ (NSString*)stringFromFrame:(CGRect)frame;
-
-//zoom a map to fit the set annotations
-+ (void)zoomToFitMapAnnotations:(MKMapView*)mapView padding:(NSInteger)padding;
+////////////////////// Array Util ///////////////////////////////////////
 
 //merge arrays removing duplicates with an object comparison
 + (NSArray*)mergeArrayWithObjectContent:(NSArray*)a1 with:(NSArray*)a2;
@@ -57,30 +59,27 @@
 //contain strings
 + (NSArray*)mergeArrayResults:(NSArray*)a1 with:(NSArray*)a2;
 
+
+
+//////////////////// String Util /////////////////////////////////////////
+
+//write data to a file with the given filename
++ (void)writeString:(NSString*)data ToFile:(NSString*)filename;
+
+//return a string with the frame params.  Useful for debugging.
++ (NSString*)stringFromFrame:(CGRect)frame;
+
+//return the string response from a string formatted url
++ (NSString*)getStringFromURLString:(NSString*)url;
+
 //returns YES if str starts with str2
 + (BOOL)string:(NSString *)str startsWithStr:(NSString *)str2;
 
 //return YES if str contains str2 (similar to java String.indexOf() function)
 + (BOOL)string:(NSString*)str containsStr:(NSString*)str2;
 
-//convert degress to radians
-+ (double)toRadians:(double)degrees;
-
-//Geo proximity query using the Haversine Equation. Proximity in miles
-//http://blog.fedecarg.com/2009/02/08/geo-proximity-search-the-haversine-equation/
-+ (BOOL)location:(CLLocation*)location isWithinProximity:(CGFloat)radius ofLocation:(CLLocation*)center;
-
-//return the zoom level for the given map rect and size of the mapview
-+ (NSUInteger)zoomLevelForMapRect:(MKMapRect)mRect withMapViewSizeInPixels:(CGSize)viewSizeInPixels;
-
 //return a string from a UTF8 char array
 + (NSString*)stringFromUTF8:(char*)c;
-
-//convert zoom scale to zoom level for a map
-+ (NSInteger)zoomScaleToZoomLevel:(MKZoomScale)scale;
-
-//returns the height of a UILabel based on the content of the label, the font and the width of the view
-+ (float)getUILabelHeightByString:(NSString*)myString withFont:(UIFont*)font viewWidth:(int)myWidth;
 
 //trim white space chars from str
 + (NSString*)trimString:(NSString*)str;
@@ -96,6 +95,26 @@
 
 //return YES if a string is nil or empty (@"").  The string is trimmed, so @" " will return YES
 + (BOOL)stringNullOrEmpty:(NSString*)str;
+
+
+
+//////////////////// Mapping /////////////////////////////////////////////
+
+//zoom a map to fit the set annotations
++ (void)zoomToFitMapAnnotations:(MKMapView*)mapView padding:(NSInteger)padding;
+
+//convert degress to radians
++ (double)toRadians:(double)degrees;
+
+//Geo proximity query using the Haversine Equation. Proximity in miles
+//http://blog.fedecarg.com/2009/02/08/geo-proximity-search-the-haversine-equation/
++ (BOOL)location:(CLLocation*)location isWithinProximity:(CGFloat)radius ofLocation:(CLLocation*)center;
+
+//return the zoom level for the given map rect and size of the mapview
++ (NSUInteger)zoomLevelForMapRect:(MKMapRect)mRect withMapViewSizeInPixels:(CGSize)viewSizeInPixels;
+
+//convert zoom scale to zoom level for a map
++ (NSInteger)zoomScaleToZoomLevel:(MKZoomScale)scale;
 
 //creates an array of retNumSeg points on a great circle arc from point1 to point2.
 //if numsegs != retNumSegs, it will return the first retNumSegs of the arc, but
@@ -115,10 +134,16 @@
                                      toPoint:(CLLocationCoordinate2D)point2
                                   forMapView:(MKMapView*)mapView;
 
+
+
+////////////////////  Date and Time  ///////////////////////////////////////
+
 //get the current Date as a string for a given timezone with the given format
 + (NSString*)displayCurrentDateForTimezone:(NSTimeZone*)timezone withFormat:(NSString*)format;
 
 //get the current Date as a string for a given timezone with the format @"yyyy-MM-dd HH:mm ZZZZ"
 + (NSString*)displayCurrentDateForTimezone:(NSTimeZone*)timezone;
 
+//format a date to a string using the given format
++ (NSString*)formatDate:(NSDate*)date withFormat:(NSString*)format;
 @end
